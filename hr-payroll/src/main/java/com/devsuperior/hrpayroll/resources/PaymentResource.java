09797d89e7cc5e9dc654219b2691dd3d9ec301dc
KaiqueSM <kaiquesmendonca@hotmail.com>
2023-03-30 08:47:11 -0300

    Configuracao de tolerancia padrao para o Hystrix

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/resources/PaymentResource.java
@@ -2,6 +2,7 @@ package com.devsuperior.hrpayroll.resources;
 
 import com.devsuperior.hrpayroll.entities.Payment;
 import com.devsuperior.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,10 +18,18 @@ public class PaymentResource {
     private PaymentService service;
 
     @GetMapping("/{workerId}/days/{days}")
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     public ResponseEntity<Payment> getPayment(
             @PathVariable Long workerId,
             @PathVariable int days
     ){
         return ResponseEntity.ok(service.getPayment(workerId, days));
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(
+            Long workerId,
+            int days
+    ){
+        return ResponseEntity.ok(new Payment("brann", 400.0, days));
     }
 }
