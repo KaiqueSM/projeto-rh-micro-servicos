e6975ef76b25930b6a6ec3e380c9900602b1b985
KaiqueSM <kaiquesmendonca@hotmail.com>
2023-04-03 15:23:07 -0300

    Login, JWT token

--- hr-user/src/main/resources/data.sql
+++ hr-user/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO tb_user (name, email, password) VALUES ('Nina Brown', 'nina@gmail.com', '$2a$10$NYFZ/8WaQ3Qb6FCs.00jce4nxX9w7AkgWVsQCG6oUwTAcZqP9Flqu');
-INSERT INTO tb_user (name, email, password) VALUES ('Leia Red', 'leia@gmail.com', '$2a$10$H6tC4y0.Yn0gqWK9hGxm/uO6q4IhcL4oViF25yqqU3Ig6X.i6Iyji');
+INSERT INTO tb_user (name, email, password) VALUES ('Leia Red', 'leia@gmail.com', '$2a$10$NYFZ/8WaQ3Qb6FCs.00jce4nxX9w7AkgWVsQCG6oUwTAcZqP9Flqu');
 
 INSERT INTO tb_role (role_name) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (role_name) VALUES ('ROLE_ADMIN');
